9148c51371836d0d9af50466b9d3884f30d8200c
Heitor Neto <hbaneto@gmail.com>
2017-11-01 14:47:08 -0200

    Projeto inicial

---
+++ A6LP2/A6LP2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="A6LP2.MvcApplication" Language="C#" %>
---
+++ A6LP2/A6LP2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(A6LP2.Startup))]
+namespace A6LP2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
